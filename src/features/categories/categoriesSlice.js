f22165c12d5e1d0abdd539849243b09551f30d7b
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-08-21 21:31:39 +0600

    Installed redux toolkit and set up it

---
+++ src/features/categories/categoriesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: {
+    items: []
+  },
+  reducers: {
+
+  }
+})
