2ca1d53ce3bda2ad930deb4fb7435e0eb22c74c5
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-08-21 20:52:48 +0600

    Deleted unnecessary code

--- src/common/Categories.jsx
+++ src/common/Categories.jsx
@@ -9,21 +9,6 @@ function Categories() {
             <img src="electronics-image.jpg" alt="Electronics" className="w-full h-32 object-cover mb-2" />
             <p className="text-gray-600">Discover the latest gadgets and tech.</p>
           </a>
-          <a href="#" className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300">
-            <h3 className="text-lg font-semibold mb-2">Jewelry</h3>
-            <img src="jewelry-image.jpg" alt="Jewelry" className="w-full h-32 object-cover mb-2" />
-            <p className="text-gray-600">Explore stunning jewelry pieces.</p>
-          </a>
-          <a href="#" className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300">
-            <h3 className="text-lg font-semibold mb-2">Men&apos;s Clothing</h3>
-            <img src="mens-clothing-image.jpg" alt="Men&apos;s Clothing" className="w-full h-32 object-cover mb-2" />
-            <p className="text-gray-600">Find stylish outfits for men.</p>
-          </a>
-          <a href="#" className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300">
-            <h3 className="text-lg font-semibold mb-2">Women&apos;s Clothing</h3>
-            <img src="womens-clothing-image.jpg" alt="Women&apos;s Clothing" className="w-full h-32 object-cover mb-2" />
-            <p className="text-gray-600">Discover the latest trends in women&apos;s fashion.</p>
-          </a>
         </div>
       </div>
     </div>
